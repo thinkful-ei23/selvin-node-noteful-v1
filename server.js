50a2ebb9acb208bd8d6c5bf3ac880254c9e95c5a
Selvin Norman <selvin715@gmail.com>
2018-07-31 14:50:59 -0700

    Express Error-Handling Middleware

--- server.js
+++ server.js
@@ -16,29 +16,46 @@ const logStuff = require('./middleware/logger');
 //app.use(morgan('dev'));
 app.use(express.static('public'));
 app.use(logStuff);
+
+app.get('/boom', (req, res, next) => {
+  throw new Error('Boom!!');
+});
+app.get('/api/notes/:id', (req, res) => {
+const id = req.params.id;
+res.json(data.find(item => item.id === Number(id)));
+
+});
+
+app.use(function (req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  res.status(404).json({ message: 'Not Found' });
+});
+
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: err
+  });
+});
+app.get('/api/notes', (req, res) => {
+    
+  const query = req.query;
+  let list = data;
+  if(query.searchTerm) {
+    list = data.filter( function(item) {
+      return item.title.includes(query.searchTerm);
+    });
+  }
+
+  res.json(list);
+
+});
+
 // Listen for incoming connections
 app.listen(PORT, function () {
   console.info(`Server listening on ${this.address().port}`);
 }).on('error', err => {
   console.error(err);
-});
-
-app.get('/api/notes', (req, res) => {
-    
-    const query = req.query;
-    let list = data;
-    if(query.searchTerm) {
-      list = data.filter( function(item) {
-        return item.title.includes(query.searchTerm);
-      });
-    }
-
-    res.json(list);
-
-
-  });
-app.get('/api/notes/:id', (req, res) => {
-  const id = req.params.id;
-  res.json(data.find(item => item.id === Number(id)));
-  
 });
